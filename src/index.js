287309e31eafc9ba292ab4e68f7c2a0ec67c9f24
Douglas Felipe da Silva <douglas.silva@unidavi.edu.br>
2022-05-19 16:59:37 -0300

    testing cycle.sh

--- src/index.js
+++ src/index.js
@@ -13,18 +13,15 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  const { title, url, techs } = request.body
-
-  const repository = {
-    id: uuid(),
-    title,
-    url,
-    techs,
-    likes: 0
-  };
-
+  const { title, url, techs } = request.body,
+        repository = {
+          id: uuid(),
+          title,
+          url,
+          techs,
+          likes: 0
+        };
   repositories.push(repository);
-
   return response.status(201).json(repository);
 });
 
